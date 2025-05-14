d5be23488f172d6aa17f37849be2735e8b814659
Victor Eduardo Saez Vivar <nod.nole@gmail.com>
2025-05-14 19:10:38 -0400

    inicio proyecto v2
    inicio proyecto v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlqFDyHsCr6OYNT2Bf8H-ykArq3FVXELw",
+    authDomain: "inveducades.firebaseapp.com",
+    projectId: "inveducades",
+    storageBucket: "inveducades.firebasestorage.app",
+    messagingSenderId: "99358423388",
+    appId: "1:99358423388:web:8e2779bcf01962ab276999",
+    measurementId: "G-Q5004MSHGR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlqFDyHsCr6OYNT2Bf8H-ykArq3FVXELw",
+    authDomain: "inveducades.firebaseapp.com",
+    projectId: "inveducades",
+    storageBucket: "inveducades.firebasestorage.app",
+    messagingSenderId: "99358423388",
+    appId: "1:99358423388:web:8e2779bcf01962ab276999",
+    measurementId: "G-Q5004MSHGR"
+  }
 };
 
 /*
