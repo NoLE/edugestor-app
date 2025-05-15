dd70293f502ea625c2e4756a98c9525c67505fd2
Victor Eduardo Saez Vivar <nod.nole@gmail.com>
2025-05-14 20:21:08 -0400

    Iconos de Ionic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.web.inveduca',
-  appName: 'EduGestor',
+  appName: 'eduGestor',
   webDir: 'www'
 };
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { FirestoreService } from '../firebase/firestore.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+
+  private firestoreService: FirestoreService = inject(FirestoreService);
+
+  constructor() {
+    this.test();
+    this.testLectura();
+  }
+
+  async test() {
+    console.log('test');
+    await this.firestoreService.createDocument('test', {hola: 'nada'})
+
+  }
+  testLectura(){
+    this.firestoreService.getDocumentsChanges('test').subscribe( res => {
+      console.log('test lectura ->', res);
+    });
+  }
 }
